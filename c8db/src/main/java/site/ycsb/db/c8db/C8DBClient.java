460bd514e7564a0bc412feec40167df1b811b28f
Tejinder <tejinder456@gmail.com>
2022-01-18 00:43:19 +0530

    Update C8DBClient.java

--- c8db/src/main/java/site/ycsb/db/c8db/C8DBClient.java
+++ c8db/src/main/java/site/ycsb/db/c8db/C8DBClient.java
@@ -177,7 +177,7 @@ public class C8DBClient extends DB {
       //User Credentials
 
       String user = props.getProperty("c8db.user", "mm@macrometa.io");
-      String password = props.getProperty("c8db.password", "Macrometa123!@#");
+      String password = props.getProperty("c8db.password", "");
 
       joinoperation = Double.parseDouble(props.getProperty(JOIN_PROPORTION_PROPERTY, JOIN_PROPORTION_PROPERTY_DEFAULT));
 
